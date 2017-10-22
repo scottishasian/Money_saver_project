8da50adfe1d2bed36d8f0b796f3e7e431bd95ef1
Kynan <k.song@blueyonder.co.uk>
2017-10-22 14:08:49 +0100

    Added update, delete and find to type.rb

--- models/type.rb
+++ models/type.rb
@@ -19,4 +19,36 @@ class Type
     @id = result['id'].to_i
   end
 
+  def self.all()
+    sql = "SELECT * FROM types"
+    values = []
+    types = SqlRunner.run(sql, values)
+    result = types.map { |type| Type.new(type)}
+    return result
+  end
+
+  def delete()
+    sql = "DELETE FROM types
+           WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def update() #Come back to this.
+    sql = "UPDATE types
+           SET (name) = ($1)
+           WHERE id = $2"
+    values = [@name, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM types
+           WHERE id = $1"
+    values = [id]
+    types = SqlRunner.run(sql, values)
+    result = Type.new(types.first)
+    return result
+  end
+
 end
